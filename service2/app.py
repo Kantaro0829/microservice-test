0277fac67a633ce409fd35976880636aebd77147
Kantaro0829 <eshangwan@gmail.com>
2022-01-11 15:35:26 +0900

    [update] update both service of Dockerfile and app.py for adapting cors

--- service2/app.py
+++ service2/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def hello_world():
     return jsonify({"status": 200, "message": "service2-version1"})
 
